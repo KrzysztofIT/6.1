1300681d852e2f607db5b524b91e7f2da87f3ccf
“KrzysztofIT <krzysztofwasilewski.it@gmail.com>
2018-05-08 20:45:30 +0200

    6.2 pierwszy commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,31 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator calc = new Calculator();
+        if (calc.add(3,5) == 8 ){
+            System.out.println("test OK");
+        }else{
+            System.out.println("Error!");
+        }
+        if (calc.subtract(8,2) == 6 ){
+            System.out.println("test OK");
+        }else{
+            System.out.println("Error!");
+        }
     }
 }
