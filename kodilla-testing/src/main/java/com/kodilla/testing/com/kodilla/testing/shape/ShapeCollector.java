303d838330202539b3364bb726cc07fb81afae1f
“KrzysztofIT <krzysztofwasilewski.it@gmail.com>
2018-05-13 14:28:18 +0200

    pierwszy commit 6.4

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/com/kodilla/testing/shape/ShapeCollector.java
@@ -0,0 +1,35 @@
+package com.kodilla.testing.com.kodilla.testing.shape;
+
+interface Shape{
+    public String getShapeName();
+    public double getField();
+
+}
+class Square implements Shape {
+    public String getShapeName(){
+        return null;
+    }
+    public double getField(){
+        return 0 ;
+    }
+}
+
+class Triangle implements Shape {
+    public String getShapeName(){
+        return null;
+    }
+    public double getField(){
+        return 0 ;
+    }
+}
+
+class Circle implements Shape {
+    public String getShapeName(){
+        return null;
+    }
+    public double getField(){
+        return 0 ;
+    }
+}
+public class shapes {
+}
