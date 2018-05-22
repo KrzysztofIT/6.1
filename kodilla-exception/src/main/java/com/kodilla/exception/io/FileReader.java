50289c3eeea2ce3ba3d60d0fd9e6ff93d37b0511
“KrzysztofIT <krzysztofwasilewski.it@gmail.com>
2018-05-22 21:44:22 +0200

    pierwszy commit 8.2

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
@@ -0,0 +1,32 @@
+package com.kodilla.exception.io;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class FileReader {
+    public void readFile(){
+
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource("file/names").getFile());
+        //Path path = Paths.get(file.getPath());
+        //Path path = Paths.get("files/names");
+
+        try (Stream<String> fileLines = Files.lines(Paths.get(file.getPath()))){
+
+            //Stream<String> fileLines = Files.lines(path);
+            fileLines.forEach(System.out::println);
+
+        } catch (IOException e) {
+
+            System.out.println("Something is not YES" + e);
+        }finally {
+
+            System.out.println("lalala");
+        }
+        //System.out.println(file.getPath());
+    }
+}
