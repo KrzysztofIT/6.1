f4210bcc2920d91ca530dde7c82c79f1a8365fd2
“KrzysztofIT <krzysztofwasilewski.it@gmail.com>
2018-07-02 22:29:48 +0200

    First attempt to Relations 13.3

---
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/invoice/Invoice.java
@@ -0,0 +1,58 @@
+package com.kodilla.hibernate.invoice;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "INVOICES")
+public class Invoice {
+    int id;
+    String number;
+    List<Item> items = new ArrayList<>();
+
+    public Invoice() {
+    }
+
+    public Invoice( String number) {
+        this.number = number;
+    }
+
+    @Id
+    @GeneratedValue
+    @NotNull
+    @Column(name = "ID", unique = true)
+    public int getId() {
+        return id;
+    }
+
+    private void setId(int id) {
+        this.id = id;
+    }
+
+    @NotNull
+    @Column(name = "INVOICE_NAME", unique = true)
+    public String getNumber() {
+        return number;
+    }
+
+    private void setNumber(String number) {
+        this.number = number;
+    }
+
+    // tu będzie join
+    @OneToMany(
+            targetEntity = Item.class,
+            mappedBy = "invoice",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+    )
+    public List<Item> getItems() {
+        return items;
+    }
+
+    private void setItems(List<Item> items) {
+        this.items = items;
+    }
+}
---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/invoice/dao/InvoiceDaoTestSuite.java
@@ -0,0 +1,56 @@
+package com.kodilla.hibernate.invoice.dao;
+
+import com.kodilla.hibernate.invoice.Invoice;
+import com.kodilla.hibernate.invoice.Item;
+import com.kodilla.hibernate.invoice.Product;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.math.BigDecimal;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InvoiceDaoTestSuite {
+    @Autowired
+    private InvoiceDao invoiceDao;
+
+    @Test
+    public void testInvoiceDaoSave() {
+        //Given
+        Product product = new Product("Pomidor");
+
+        Item item1 = new Item( new BigDecimal(20), 13, new BigDecimal(30));
+        Item item2 = new Item( new BigDecimal(40), 132, new BigDecimal(4000));
+
+        Invoice invoice =  new Invoice("2018/01" );
+
+        item1.setProduct(product);
+        item2.setProduct(product);
+        item1.setInvoice(invoice);
+        item2.setInvoice(invoice);
+
+        System.out.println(item1.getValue());
+        System.out.println(item1 == null);
+        System.out.println(invoice.getItems() == null);
+
+        invoice.getItems().add(item1);
+        invoice.getItems().add(item2);
+
+
+
+
+        //When
+        invoiceDao.save(invoice);
+        int id = invoice.getId();
+
+        //Then
+        Assert.assertNotEquals(0, id);
+
+        //CleanUp
+        invoiceDao.deleteById(id);
+    }
+}
