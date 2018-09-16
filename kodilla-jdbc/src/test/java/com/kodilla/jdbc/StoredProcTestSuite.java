4ccbd45150cf8d1146049968695a062c197cb064
“KrzysztofIT <krzysztofwasilewski.it@gmail.com>
2018-09-16 18:13:56 +0200

    first attempt to 22.3

---
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
@@ -0,0 +1,53 @@
+package com.kodilla.jdbc;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
+public class StoredProcTestSuite {
+    @Test
+    public void testUpdateVipLevels() throws SQLException {
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+        //String sqlUpdate = "UPDATE READERS SET VIP_LEVEL=\"Not set\"";
+        Statement statement= dbManager.getConnection().createStatement();
+        //statement.executeUpdate(sqlUpdate);
+        //When
+
+        //Then
+        String sqlCheckTable = "SELECT COUNT(*) AS HOW_MANY FROM READERS WHERE VIP_LEVEL=\"Not set\"";
+        ResultSet rs = statement.executeQuery(sqlCheckTable);
+        String sqlProcedureCall = "CALL UpdateVipLevels()";
+        //statement.execute(sqlProcedureCall);
+        int howMany = -1 ;
+        if (rs.next()) {
+            howMany = rs.getInt("HOW_MANY");
+        }
+        Assert.assertEquals(0,howMany);
+
+        }
+    @Test
+    public void testUpdateBestsellers() throws SQLException {
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+        //String sqlUpdate = "UPDATE READERS SET VIP_LEVEL=\"Not set\"";
+        Statement statement= dbManager.getConnection().createStatement();
+        //statement.executeUpdate(sqlUpdate);
+        //When
+
+        //Then
+        String sqlCheckTable = "SELECT COUNT(*) AS HOW_MANY FROM BOOKS WHERE BESTSELLER=false";
+        ResultSet rs = statement.executeQuery(sqlCheckTable);
+        String sqlProcedureCall = "CALL UpdateBestsellers()";
+        //statement.execute(sqlProcedureCall);
+        int howMany = -1 ;
+        if (rs.next()) {
+            howMany = rs.getInt("HOW_MANY");
+        }
+        Assert.assertEquals(4,howMany);
+
+    }
+}
