16c697c9887ed56eac3a47be8543e48b9baaa44c
“KrzysztofIT <krzysztofwasilewski.it@gmail.com>
2018-09-17 08:05:53 +0200

    second attempt tp 22.3

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
@@ -12,16 +12,15 @@ public class StoredProcTestSuite {
     public void testUpdateVipLevels() throws SQLException {
         //Given
         DbManager dbManager = DbManager.getInstance();
-        //String sqlUpdate = "UPDATE READERS SET VIP_LEVEL=\"Not set\"";
+        String sqlUpdate = "UPDATE READERS SET VIP_LEVEL=\"Not set\"";
         Statement statement= dbManager.getConnection().createStatement();
-        //statement.executeUpdate(sqlUpdate);
+        statement.executeUpdate(sqlUpdate);
         //When
-
+        String sqlProcedureCall = "CALL UpdateVipLevels()";
+        statement.execute(sqlProcedureCall);
         //Then
         String sqlCheckTable = "SELECT COUNT(*) AS HOW_MANY FROM READERS WHERE VIP_LEVEL=\"Not set\"";
         ResultSet rs = statement.executeQuery(sqlCheckTable);
-        String sqlProcedureCall = "CALL UpdateVipLevels()";
-        //statement.execute(sqlProcedureCall);
         int howMany = -1 ;
         if (rs.next()) {
             howMany = rs.getInt("HOW_MANY");
@@ -33,16 +32,15 @@ public class StoredProcTestSuite {
     public void testUpdateBestsellers() throws SQLException {
         //Given
         DbManager dbManager = DbManager.getInstance();
-        //String sqlUpdate = "UPDATE READERS SET VIP_LEVEL=\"Not set\"";
+        String sqlUpdate = "UPDATE READERS SET VIP_LEVEL=\"Not set\"";
         Statement statement= dbManager.getConnection().createStatement();
-        //statement.executeUpdate(sqlUpdate);
+        statement.executeUpdate(sqlUpdate);
         //When
-
+        String sqlProcedureCall = "CALL UpdateBestsellers()";
+        statement.execute(sqlProcedureCall);
         //Then
         String sqlCheckTable = "SELECT COUNT(*) AS HOW_MANY FROM BOOKS WHERE BESTSELLER=false";
         ResultSet rs = statement.executeQuery(sqlCheckTable);
-        String sqlProcedureCall = "CALL UpdateBestsellers()";
-        //statement.execute(sqlProcedureCall);
         int howMany = -1 ;
         if (rs.next()) {
             howMany = rs.getInt("HOW_MANY");
