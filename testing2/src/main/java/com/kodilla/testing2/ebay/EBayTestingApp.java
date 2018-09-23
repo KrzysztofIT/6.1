ce6ed8310f8253491609ca7b0bb25b684dd5d259
“KrzysztofIT <krzysztofwasilewski.it@gmail.com>
2018-09-23 14:38:08 +0200

    second alternative  attempt to 23.2

--- testing2/src/main/java/com/kodilla/testing2/ebay/EBayTestingApp.java
+++ testing2/src/main/java/com/kodilla/testing2/ebay/EBayTestingApp.java
@@ -6,13 +6,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class EBayTestingApp {
-    public static final String SEARCHFIELD = "gh-ac";
+   //public static final String SEARCHFIELD = "gh-ac";
+   public static final String SEARCHFIELD = "_nkw";
 
     public static void main(String[] args){
         WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
         driver.get("https://www.ebay.com/");
 
-        WebElement searchfield = driver.findElement(By.id(SEARCHFIELD));
+        //WebElement searchfield = driver.findElement(By.id(SEARCHFIELD));
+        WebElement searchfield = driver.findElement(By.name(SEARCHFIELD));
         searchfield.sendKeys("Laptop");
         searchfield.submit();
     }
